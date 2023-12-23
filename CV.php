405229ddee95056c4709b63c1247f87d82a31df8
NgHung24 <hung97295@gmail.com>
2023-12-23 22:41:54 +0700

    update

--- CV.php
+++ CV.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>! Giới thiệu bản thân đi nào , lên thôi nào!</title>
+	<title>! Giới thiệu bản thân đi nào lên thôi nào!</title>
 </head>
 <body>
 
