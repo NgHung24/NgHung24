47ca67a50ec44465384f1b1587f4877ee662295f
NgHung24 <hung97295@gmail.com>
2023-12-23 16:58:53 +0700

    add source

---
+++ CV.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>! Giới thiệu bản thân đi nào !</title>
+</head>
+<body>
+
+<?php 
+
+echo "<h1 style = 'color: orange', align = 'center'><u>Giới Thiệu Về  Bản Thân và Gia Đình </u></h1>";
+
+echo 
+"
+<ul tyle = square >
+	<li> <b>Thông tin bản thân: </b>
+			<blockquote>Họ Tên: Nguyễn Ngọc Hùng<br>
+						Ngày sinh: 30/8/2004 <br>
+						GT: Nam. <br>
+						SDT: 0973244362<br>
+						Quê: Đại Từ - Thái Nguyên<br>
+						Nghề Nghiệp: Sinh Viên	
+
+			</blockquote>
+			<i>Đang là sinh viên trường Đại học Công Nghệ GTVT.</i><br>
+
+	<li> <b>Thông tin Gia Đình: </b>
+	<li><i>Thông tin của mẹ</i>: 		
+			<blockquote>
+				Họ Tên: Trân Văn B <br>
+				Ngày sinh: 3/3/1970<br>
+				SDT: 0977507859<br>
+				Nghề Nghiệp: Công chức nhà nước
+			</blockquote>
+	<li><i>Thông tin bố</i>:
+			<blockquote>
+				Họ Tên: Nguyên Tài N<br>
+				Ngày sinh: 20/10/1970<br>
+				SDT: 0966524866<br>
+				Nghề Nghiệp: Bộ Đội
+			</blockquote>
+	<li><i> Thông tin em trai:</i>
+			<blockquote>
+				Họ tên: Nguyễn Lam H<br>
+				Ngày sinh: 12/1/2010<br>
+				Nghề Nghiệp: Học Sinh
+			</blockquote>
+
+</ul>
+"
+;
+ echo "<a href = 'helo.php' > Thông tin thêm:.. </a>";
+
+
+ ?>
+
+
+
+</body>
+</html>
